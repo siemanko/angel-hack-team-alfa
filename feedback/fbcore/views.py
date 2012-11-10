83cd5b997f499bb31b5fda46a973c06fd9691e35
sidor <szymon.sidor@gmail.com>
2012-11-10 14:39:34 +0000

    First page working: go to /core/

---
+++ feedback/fbcore/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import Context, loader, RequestContext
+
+def index(request):
+    return render_to_response('fbcore/index.html', {})
