0feb4a1367196729c890ab84e169af9af79cb6ce
sidor <szymon.sidor@gmail.com>
2012-11-10 17:36:46 +0000

    Fix for admin styles

--- feedback/feedback/settings.py
+++ feedback/feedback/settings.py
@@ -61,18 +61,17 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = PROJECT_DIR + '/sitestatic'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://localhost:8000/static/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    PROJECT_DIR + 'static'
 )
 
 # List of finder classes that know how to find static files in
@@ -81,6 +80,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    PROJECT_DIR + 'static'
 )
 
 # Make this unique, and don't share it with anybody.
