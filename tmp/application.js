c1ba2f7707bc12a512520224baef0996074e53e2
Crazy Rabbit Lab <crazyrabbitlab@hiob.(none)>
2012-11-10 17:55:38 +0100

    test JSON

--- tmp/application.js
+++ tmp/application.js
@@ -8,34 +8,30 @@ var apptime = 1;
 var nextUpdate = 1000;
 function heartbeat() {
 	apptime += 10;
+	var addr = $("#addr").val();
 	if (apptime > nextUpdate && nextUpdate > 0) {
 		nextUpdate = -1;
 		$.ajax({
-			url : "studentq/getstate",
-			dataType : "html",
+			url : addr,
+			dataType : "json",
 			success: function(data) {
 				appUpdateProc(data);
-				nextUpdate = apptime + 2000;
+				nextUpdate = apptime + 20000;
 			},
 			error : function(jqXHR, textStatus, errorThrown) {
 				appUpdateProc(undefined, errorThrown);
-				nextUpdate = apptime + 5000;
+				nextUpdate = apptime + 50000;
 			}
 		});
 	}
 	setTimeout(heartbeat, 10);
 }
 
-function updateApplicationState() {
-}
-
-function updateApplicationStateMock(data, error) {
-	$("#callNo").html("Call: "+apptime);
-	if (error) {
-		$("#appstate").html("ERROR! "+error);
-	} else {
-		$("#appstate").html(data);
-	}
+function updateApplicationState(data) {
+	var state = data.state;
+	var questions = state.questions;
+	questions.sort(function(q1,q2) { return q2.votescore - q1.votescore; });
+	
 }
 
 function addQuestion(text) {
@@ -46,4 +42,38 @@ function voteQuestion() {
 
 function updateNow() {
 	nextUpdate = apptime+1;
+}
+
+
+// Mocks
+
+function updateApplicationStateMock(data, error) {
+	$("#callNo").html("Call: "+apptime);
+	if (error) {
+		$("#appstate").html("ERROR! "+error);
+	} else {
+		$("#appstate").html(data);
+	}
+}
+
+function updateApplicationStateMock2(data, error) {
+	updateApplicationStateMock(data, error);
+	
+	if (data) {
+		updateApplicationStateMock(data);
+	}
+}
+
+function updateApplicationStateMock(data) {
+	var state = data.state;
+	var questions = state.questions;
+	
+	questions.sort(function(q1,q2) { return q2.votescore - q1.votescore; });
+	
+	$("#window").html("");
+	var ul = $("<ul />");
+	questions.forEach(function(question) {
+		ul.append("<li>"+question.text+"</li>");
+	});
+	$("#window").append(ul);
 }
